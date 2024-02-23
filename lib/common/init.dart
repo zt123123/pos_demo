49628d511c360d6ee7f935d155b5900cc1676a6d
zhangtian <zhangtian@weidian.com>
2024-02-23 15:48:25 +0800

    chore: comment

--- lib/common/init.dart
+++ lib/common/init.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void initApp(Function() initCallback) {
+void initApp([Function()? initCallback]) {
   // DB Storage Settings init...
+  // 隐藏状态栏和导航栏
   WidgetsFlutterBinding.ensureInitialized();
-  // 隐藏状态栏和导航栏  上方黑板
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-  initCallback();
+  initCallback?.call();
 }
